70e2d30845e4b8deff0bef9203be4f773ee13fe0
robin <robinson_c@yahoo.com>
2016-08-10 13:58:01 +0800

    Fix server error

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 var express = require("express");
 var app = express();
-var __dirname = "../connect";
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/'));
-
 app.get('/', function (req, res) {
   res.sendFile('index.html', {"root": __dirname});
 });
 
 app.listen(port);
+console.log('__dirname:', __dirname);
 console.log("Running at Port " + port);
